6d7782c7915dbd5cd84fc6fce6a4cd6ffc9a007e
Leyla Alekperova <leslairy@gmail.com>
2018-08-08 20:29:49 +0000

    Done.

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,14 @@
-#write out your code here
-
 def least_coins(cents)
+x=67388438349
+y=x/25
+a=x%25
 
-#Code your answer here!
-
+b=a/10
+l=a%10
+m=l/5
+z=l%5 
+o=z/1
+final=y+b+m+o  
+ puts final
 end
+least_coins(67388438349)
